d84a528c053a1ef4c09d456f2252a05329a68d63
keisukethewinter <addict20fx1@gmail.com>
2020-08-18 12:41:22 +0900

    既読機能のサーバーサイド実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   root to: 'posts#index'
-  # get 'posts', to: 'posts#index'
-  # get 'posts/new', to: 'posts#new'
   post 'posts', to: 'posts#create'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  #  For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # get 'posts', to: 'posts#checked'#queryパラメータで記述。既読機能に必要なパラメーターは、「どのメモを既読したか」を判別するためのメモのidです。
+                                    #メモのidを取得できるようにルーティングに設定します。
+                                    # queryパラメーターを使用すると、/posts/?id=1とリクエストを行うと、params[:id]としてパラメーターを取得することができます。
+  get 'posts/:id', to:'posts#checked'#pathパラメータで記述。今回のように、postのidであれば'posts/:id'のように記載するpathパラメーターの方が認識もしやすく、記述も単純です。
 end
